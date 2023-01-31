8d6158bccd0f88a439d0f6058e2200b714b33551
bashisha <bernie86@gmail.com>
2023-01-30 22:43:27 -0500

    Add project files.

---
+++ AzureDevOps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureDevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Milli!");
+        }
+    }
+}
